0ef2c7486ff90c50480310537b5a843dae90c473
Alexander von Below <Alex@vonBelow.Com>
2013-11-07 11:35:56 +0100

    Initial project

---
+++ EXC_BAD_WEBVIEW/EXC_BAD_WEBVIEW.h
@@ -0,0 +1,13 @@
+//
+//  EXC_BAD_WEBVIEW.h
+//  EXC_BAD_WEBVIEW
+//
+//  Created by Alexander v. Below on 07.11.13.
+//  Copyright (c) 2013 Alexander von Below. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EXC_BAD_WEBVIEW : NSObject
+
+@end
---
+++ EXC_BAD_WEBVIEW/EXC_BAD_WEBVIEW.m
@@ -0,0 +1,13 @@
+//
+//  EXC_BAD_WEBVIEW.m
+//  EXC_BAD_WEBVIEW
+//
+//  Created by Alexander v. Below on 07.11.13.
+//  Copyright (c) 2013 Alexander von Below. All rights reserved.
+//
+
+#import "EXC_BAD_WEBVIEW.h"
+
+@implementation EXC_BAD_WEBVIEW
+
+@end
